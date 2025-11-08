9adb8d34021d24535f4248b77d3f69486b4c5193
Kevin Abarca <kevinabarca1224@gmail.com>
2025-11-08 15:36:29 -0600

    Primer commit. Menú lateral y mapa del mundo

---
+++ Proyecto2_Datos_I/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using SideBar_Nav;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto2_Datos_I
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            navframe.Navigate(new Uri("Pages/Page4.xaml", UriKind.RelativeOrAbsolute));
+
+        }
+
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.NavLink);
+        }
+        private void Imagen_Click(object sender, MouseButtonEventArgs e)
+        {
+            navframe.Navigate(new Uri("Pages/Page4.xaml", UriKind.RelativeOrAbsolute));
+          
+
+
+        }
+    }
+}
